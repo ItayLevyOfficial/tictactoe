1d7a1eb44d529dd35e84686f3b0008875d4f2fd4
Itay Levy <itaylevy134@gmail.com>
2020-03-03 23:03:29 +0200

    finished until 'Adding Time Travel'

--- src/index.js
+++ src/index.js
@@ -11,6 +11,27 @@ function Square(props) {
 }
 
 class Board extends React.Component {
+  calculateWinner() {
+    const squares = this.state.squares;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
   constructor(props) {
     super(props);
     this.state = {
@@ -29,8 +50,11 @@ class Board extends React.Component {
   }
   
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    
+    const status = (
+      this.calculateWinner() ?
+        `Winner: ${this.calculateWinner()}` :
+        `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    );
     return (
       <div>
         <div className="status">{status}</div>
@@ -55,6 +79,9 @@ class Board extends React.Component {
   
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.calculateWinner() || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState(
       {
@@ -87,3 +114,4 @@ ReactDOM.render(
   <Game/>,
   document.getElementById('root')
 );
+
