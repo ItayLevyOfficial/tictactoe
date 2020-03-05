728c6b06c06ded69693136eb7b74a527c1e4521f
Itay Levy <itaylevy134@gmail.com>
2020-03-05 21:43:40 +0200

    game component rendering status

--- src/index.js
+++ src/index.js
@@ -11,40 +11,12 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  calculateWinner() {
-    const squares = this.state.squares;
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
   
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
@@ -93,18 +65,62 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      xIsNext: true
+    }
+  }
+  
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = this.calculateWinner();
+    const status = (
+      winner ?
+        `Winner ${winner}` :
+        `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    );
     return (
       <div className="game">
         <div className="game-board">
-          <Board/>
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
+  }
+  
+  calculateWinner() {
+    const squares = this.state.history[this.history.length - 1].squares;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 }
 
