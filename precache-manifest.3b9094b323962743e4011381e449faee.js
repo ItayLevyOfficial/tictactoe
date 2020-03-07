9d1b97bcd738560879aeadb03a77129e6e5c5823
Itay Levy <itaylevy134@gmail.com>
2020-03-07 19:33:16 +0200

    Updates

---
+++ precache-manifest.3b9094b323962743e4011381e449faee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abe27f47dd48d6865fd2b2699d2c9a6",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "e0e7678bd7067c708071",
+    "url": "/tictactoe/static/css/main.278145d6.chunk.css"
+  },
+  {
+    "revision": "36b4bd02d50300c0504d",
+    "url": "/tictactoe/static/js/2.f4075122.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tictactoe/static/js/2.f4075122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e7678bd7067c708071",
+    "url": "/tictactoe/static/js/main.48329bd7.chunk.js"
+  },
+  {
+    "revision": "ceecb7b45847aa4147c0",
+    "url": "/tictactoe/static/js/runtime-main.b6f03ea1.js"
+  }
+]);
