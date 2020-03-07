cdd10a82baa186f6084a8c8cd5f5c5e7d50ccda1
Itay Levy <itaylevy134@gmail.com>
2020-03-07 22:28:31 +0200

    Created renderRow function for shorter code.

--- src/index.js
+++ src/index.js
@@ -13,30 +13,30 @@ function Square(props) {
 class Board extends React.Component {
   renderSquare(i) {
     return (
-      <Square
+      <Square key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
     )
   }
   
+  renderRow(rowNumber) {
+    return Array.from(
+      Array(3),
+      (_, index) => this.renderSquare(3 * rowNumber + index));
+  }
+  
   render() {
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderRow(0)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderRow(1)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderRow(2)}
         </div>
       </div>
     );
